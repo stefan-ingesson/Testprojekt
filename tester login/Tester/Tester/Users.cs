6c97343127136d8c338345829af2a90d45ef1f35
Stefan Ingesson <stefan.ingesson@gmail.com>
2015-09-30 15:05:38 +0200

    2 första test klara

---
+++ tester login/Tester/Tester/Users.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tester
+{
+   public class Users
+    {
+        //public string Name { get; set; }
+        //public string PassWord { get; set; }
+
+
+       public string Name = "Abdullah";
+       public string PassWord = "Moreno";
+        //List<Users> _users = new List<Users>();
+
+        //public Users()
+        //{
+        //    Users user = new Users();        
+
+        //    user.Name = "Abdullah";
+        //    user.PassWord = "Moreno";
+
+        //    _users.Add(user);
+        //}
+
+
+    }
+
+    
+}
---
+++ tester login/Tester/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Tester;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        //Kunna kontrollera om ett användarnamn och lösenordspar är giltigt. Returnerar true eller false.
+        [TestMethod]
+        public void Control_That_Name_And_Password_Is_Viable_Return_True()
+        {
+            //Assert 
+           //var user = new Tester.Users();
+            //var name = user.Name;
+            
+          Users t = new Users();
+          var name = t.Name;
+          var password = t.PassWord;
+          Assert.IsTrue("Abdullah" == name && "Moreno" == password);
+
+        }
+
+        [TestMethod]
+        public void Control_That_Name_And_Password_Is_Viable_Return_False()
+        {
+            Users t = new Users();
+            var name = t.Name;
+            var password = t.PassWord;
+            Assert.IsFalse("Fakename" == name && "FakeLastName" == password);
+        }
+    }
+}
